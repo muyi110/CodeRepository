77aff22161bf73172c0572b04affcb4c55f03b36
muyi110 <ylqing5470@126.com>
2018-01-15 23:36:02 +0800

    commit: 2018-01-15. by---YLQ

--- PythonProject/AlienInvasion/alien_invasion.py
+++ PythonProject/AlienInvasion/alien_invasion.py
@@ -1,6 +1,32 @@
-import sys
-
 import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(ai_settings, screen)
+
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+
+    #开始游戏主循环
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+     	#让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
